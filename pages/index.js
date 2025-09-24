f36c6543a8cb70c54baf13410e2ee883687a7266
lvgroup1 <142797952+lvgroup1@users.noreply.github.com>
2025-09-24 13:14:58 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-import DamageIntakeApp from '../components/DamageIntakeApp'
+import dynamic from 'next/dynamic'
+
+// Dinamiski ielādējam komponenti tikai klientā (bez SSR),
+// citādi 'xlsx' var radīt kļūdas servera pusē.
+const DamageIntakeForm = dynamic(
+  () => import('../components/DamageIntakeForm'),
+  { ssr: false }
+)
 
 export default function Home() {
-  return <DamageIntakeApp />
+  return <DamageIntakeForm />
 }
