0e242bf72cd2ea5be4228fe6eb9fc6519e282dbd
lvgroup1 <142797952+lvgroup1@users.noreply.github.com>
2026-01-15 08:47:33 +0000

    Fix GitHub Pages paths (basePath) + export

--- next.config.js
+++ next.config.js
@@ -4,8 +4,10 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  assetPrefix: isProd ? "/eksperti" : "",
+  basePath: isProd ? "/eksperti" : "",
+  assetPrefix: isProd ? "/eksperti/" : "",
   images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
+
